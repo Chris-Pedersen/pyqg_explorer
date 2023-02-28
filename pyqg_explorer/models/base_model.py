e5bad78a918b7c8de1dc0941fe3564cdcfb4c496
Chris-Pedersen <c.pedersen@nyu.edu>
2023-02-27 20:26:12 -0500

    Add resnets

--- pyqg_explorer/models/base_model.py
+++ pyqg_explorer/models/base_model.py
@@ -151,10 +151,10 @@ class AndrewCNN(BaseModel):
         s=torch.cat((s_upper,s_lower)).detach().numpy().astype(np.double)
         return s
 
-
-def GetResBlock(in_channels,intermediate_channels,kernel_size=3):
+####################### ResNet architectures #######################
+def GetConvBlock(in_channels,intermediate_channels,out_channels,kernel_size=3):
     conv1=nn.Conv2d(in_channels,intermediate_channels,kernel_size,padding="same",padding_mode="circular")
-    conv2=nn.Conv2d(intermediate_channels,in_channels,kernel_size,padding="same",padding_mode="circular")
+    conv2=nn.Conv2d(intermediate_channels,out_channels,kernel_size,padding="same",padding_mode="circular")
     block=[conv1]
     block.append(nn.ReLU())
     block.append(nn.BatchNorm2d(intermediate_channels))
@@ -163,23 +163,125 @@ def GetResBlock(in_channels,intermediate_channels,kernel_size=3):
 
 
 class ResidualBlock(nn.Module):
+    """ Stacks 2 convolutional layers with a residual connection, forming a single
+        residual block """
     def __init__(self,in_channels,intermediate_channels,out_channels,kernel_size):
         super().__init__()
-        self.block=nn.ModuleList([*GetResBlock(in_channels,intermediate_channels,kernel_size)])
+        self.conv3x3=nn.ModuleList([*GetConvBlock(in_channels,intermediate_channels,out_channels,kernel_size)])
         self.batchnorm=nn.BatchNorm2d(in_channels)
-        self.relu=nn.ReLU
         
     def forward(self,x):
         residual=x
-        for layer in self.block:
+        for layer in self.conv3x3:
             x=layer(x)
         x+=residual
         x=torch.nn.functional.relu(x)
         x=self.batchnorm(x)
         return x
 
+    
+class NarrowResBlock(nn.Module):
+    """ Stacks 2 convolutional layers with a residual connection, forming a single
+        residual block """
+    def __init__(self,in_channels,intermediate_channels,out_channels,kernel_size):
+        super().__init__()
+        self.cut_idx=int(out_channels/2)
+        self.conv3x3=nn.ModuleList([*GetConvBlock(in_channels,intermediate_channels,out_channels,kernel_size)])
+        self.batchnorm=nn.BatchNorm2d(in_channels)
+        
+    def forward(self,x):
+        residual=x
+        for layer in self.conv3x3:
+            x=layer(x)
+        ## "Narrow" residual connection
+        x=torch.cat(((x[:,:self.cut_idx,:,:]+residual[:,0,:,:].unsqueeze(1)),x[:,self.cut_idx:,:,:]+residual[:,0,:,:].unsqueeze(1)),dim=1)
+        x=torch.nn.functional.relu(x)
+        x=self.batchnorm(x)
+        return x
+    
+    
+class ResNetParallel(base_model.BaseModel):
+    """ Resnet where the residual blocks run in parallel, after a joint convolutional first layer """
+    def __init__(self,config,model_beta=None):
+        super().__init__(config,model_beta)
+        self.num_res_blocks=config["residual_blocks"]
+        self.conv_filters=config["conv_filters"]
+        self.first_conv=nn.Conv2d(config["input_channels"],128,kernel_size=3,padding="same",padding_mode="circular")
+        self.batchnorm1=nn.BatchNorm2d(128)
+        self.second_conv=nn.Conv2d(128,self.conv_filters,kernel_size=3,padding="same",padding_mode="circular")
+        self.batchnorm2=nn.BatchNorm2d(self.conv_filters)
+        self.resnets_1=nn.ModuleList()
+        self.resnets_2=nn.ModuleList()
+        for aa in range(self.num_res_blocks):
+            self.resnets_1.append(ResidualBlock(self.conv_filters,self.conv_filters,self.conv_filters,3))
+            self.resnets_2.append(ResidualBlock(self.conv_filters,self.conv_filters,self.conv_filters,3))
+        self.final_conv_1=nn.Conv2d(self.conv_filters,1,kernel_size=3,padding="same",padding_mode="circular")
+        self.final_conv_2=nn.Conv2d(self.conv_filters,1,kernel_size=3,padding="same",padding_mode="circular")
+        
+    def forward(self,x):
+        residual=x
+        x=self.first_conv(x)
+        x=self.batchnorm1(x)
+        x=torch.nn.functional.relu(x)
+        x=self.second_conv(x)
+        x=self.batchnorm2(x)
+        
+        x_1=x+residual[:,0,:,:].unsqueeze(1)
+        x_2=x+residual[:,1,:,:].unsqueeze(1)
+        
+        x_1=torch.nn.functional.relu(x_1)
+        x_2=torch.nn.functional.relu(x_2)
+        
+        for layer_index in range(len(self.resnets_1)):
+            x_1=self.resnets_1[layer_index](x_1)
+            x_2=self.resnets_2[layer_index](x_2)
+            
+        x_1=self.final_conv_1(x_1)
+        x_2=self.final_conv_2(x_2)
+        
+        x=torch.cat((x_1,x_2),dim=1)
+        
+        return x
+    
+    
+class ResNetSingle(base_model.BaseModel):
+    """ Resnet with a single channel of residual blocks, splitting each filter set into upper and lower
+        residual connections """
+    def __init__(self,config,model_beta=None):
+        super().__init__(config,model_beta)
+        self.num_res_blocks=config["residual_blocks"]
+        self.conv_filters=config["conv_filters"]
+        self.first_conv=nn.Conv2d(config["input_channels"],128,kernel_size=3,padding="same",padding_mode="circular")
+        self.batchnorm1=nn.BatchNorm2d(128)
+        self.second_conv=nn.Conv2d(128,self.conv_filters,kernel_size=3,padding="same",padding_mode="circular")
+        self.batchnorm2=nn.BatchNorm2d(self.conv_filters)
+        self.resnets=nn.ModuleList()
+        for aa in range(self.num_res_blocks):
+            self.resnets.append(NarrowResBlock(self.conv_filters,self.conv_filters,self.conv_filters,3))
+        self.final_conv=nn.Conv2d(64,2,kernel_size=3,padding="same",padding_mode="circular")
+        
+    def forward(self,x):
+        residual=x
+        x=self.first_conv(x)
+        x=self.batchnorm1(x)
+        x=torch.nn.functional.relu(x)
+        x=self.second_conv(x)
+        x=self.batchnorm2(x)
+        
+        ## Upper layer residual connection for the first half of the filters
+        ## Lower layer for the second half
+        x=torch.cat(((x[:,:int(self.conv_filters/2),:,:]+residual[:,0,:,:].unsqueeze(1)),x[:,int(self.conv_filters/2):,:,:]+residual[:,0,:,:].unsqueeze(1)),dim=1)
+        for layer_index in range(len(self.resnets)):
+            x=self.resnets[layer_index](x)
+            
+        x=self.final_conv(x)
+        
+        return x
 
-class ResNet(BaseModel):
+
+class ResNetChoke(BaseModel):
+    """ Resnet where each residual block is collapsed down to two channels, one for upper and one for
+        lower layer """
     def __init__(self,config,model_beta=None):
         super().__init__(config,model_beta)
         self.network=nn.ModuleList([])
