121cf6a161a0e4ace25140ce36c1c34dee990167
Chris-Pedersen <c.pedersen@nyu.edu>
2023-03-13 20:59:19 -0400

    Remove duplicate code, inherit base class

--- pyqg_explorer/dataset/forcing_dataset.py
+++ pyqg_explorer/dataset/forcing_dataset.py
@@ -117,7 +117,7 @@ class ResidualDataset(BaseDataset):
         return (x_out,y_out)
 
 
-class OfflineDataset(Dataset):
+class OfflineDataset(BaseDataset):
     """
     Dataset to prepare q, f and s fields for some given time horizon
     """
@@ -184,79 +184,8 @@ class OfflineDataset(Dataset):
         y_out=torch.stack((s_upper,s_lower),dim=0)
         return (x_out,y_out)
 
-class ResidualDataset(Dataset):
-    """
-    Dataset to prepare q, f and s fields for some given time horizon
-    """
-    def __init__(self,file_path,seed=42,subsample=None,drop_spin_up=False,train_ratio=0.75,valid_ratio=0.25,test_ratio=0.0):
-        """
-        file_path:     path to data
-        seed:          random seed used to create train/valid/test splits
-        normalise:     "proper" to normalise fields to zero mean and unit variance
-        subsample:     None or int: if int, subsample the dataset to a total of N=subsample maps
-        drop_spin_up:  Drop all snapshots taken during the spin-up phase
-        train_ratio:   proportion of dataset to use as training data
-        valid_ratio:   proportion of dataset to use as validation data
-        test_ratio:    proportion of dataset to use as test data
-        
-        """
-        super().__init__()
-        
-        self.drop_spin_up=drop_spin_up
-        data_full=xr.open_dataset(file_path)
-        if self.drop_spin_up:
-            data_full=data_full.sel(time=slice(100800000.0,3.096036e+08))
-        
-        def concat_arrays(xarray_subdata):
-            def collapse_and_reshape(xarray):
-                return torch.tensor(xarray.stack(snapshot=("run","time")).transpose("snapshot","lev","y","x").data)
-            channel_index=1
-            return torch.cat([collapse_and_reshape(xarray) for xarray in xarray_subdata], channel_index)
-        
-        all_data=concat_arrays([data_full.q])
-        split_idx=np.arange(0,len(all_data),2)
-        self.x_data=all_data[split_idx]
-        self.y_data=all_data[split_idx+1]-all_data[split_idx]
-        
-        
-        ## Subsample datasets if required
-        if self.subsample:
-            self._subsample()
-        
-        self.train_ratio=train_ratio
-        self.valid_ratio=valid_ratio
-        self.test_ratio=test_ratio
-        self.rng = np.random.default_rng(seed)
 
-        self.len=len(self.x_data)
-        
-        assert len(self.x_data)==len(self.y_data), "Number of x and y samples should be the same"
-        
-        self._get_split_indices()
-        
-        ## Get means, stds for preprocessing
-        self.q_mean_upper,self.q_mean_lower=self.x_data.mean(dim=[0,2,3])
-        self.q_std_upper,self.q_std_lower=self.x_data.std(dim=[0,2,3])
-        self.res_mean_upper,self.res_mean_lower=self.y_data.mean(dim=[0,2,3])
-        self.res_std_upper,self.res_std_lower=self.y_data.std(dim=[0,2,3])      
-        
-    def __len__(self):
-        return self.len
-    
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        ## Return normalised arrays
-        q_upper=transforms.normalise_field(self.x_data[idx][0],self.q_mean_upper,self.q_std_upper)
-        q_lower=transforms.normalise_field(self.x_data[idx][1],self.q_mean_lower,self.q_std_lower)
-        res_upper=transforms.normalise_field(self.y_data[idx][0],self.res_mean_upper,self.res_std_upper)
-        res_lower=transforms.normalise_field(self.y_data[idx][1],self.res_mean_lower,self.res_std_lower)
-        x_out=torch.stack((q_upper,q_lower),dim=0)
-        y_out=torch.stack((res_upper,res_lower),dim=0)
-        return (x_out,y_out)
-
-
-class EmulatorDataset(Dataset):
+class EmulatorDataset(BaseDataset):
     """
     x_data is q_i, y_data is q_{i+dt}
     """
@@ -325,7 +254,7 @@ class EmulatorDataset(Dataset):
         return (x_out,y_out)
 
 
-class TimestepDataset(Dataset):
+class TimestepDataset(BaseDataset):
     """
     Dataset to prepare q, f and s fields for some given time horizon
     """
