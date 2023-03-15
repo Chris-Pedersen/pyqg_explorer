37fc613a1012c0422aeeb43df43a33ab0c341c70
Chris-Pedersen <c.pedersen@nyu.edu>
2023-03-15 18:58:25 -0400

    Remake Linear ResNet into a "Pre-Activation" ResNet

--- pyqg_explorer/models/resnet.py
+++ pyqg_explorer/models/resnet.py
@@ -51,7 +51,26 @@ class NarrowResBlock(nn.Module):
         x=torch.nn.functional.relu(x)
         x=self.batchnorm(x)
         return x
-    
+
+
+class LinearResidualBlock(nn.Module):
+    """ Stacks 2 convolutional layers with a residual connection, forming a single
+        residual block. Here the nonlinearity and batchnorm is applied *before*
+        the residual connection """
+    def __init__(self,in_channels,intermediate_channels,out_channels,kernel_size):
+        super().__init__()
+        self.conv3x3=nn.ModuleList([*GetConvBlock(in_channels,intermediate_channels,out_channels,kernel_size)])
+        self.batchnorm=nn.BatchNorm2d(in_channels)
+        
+    def forward(self,x):
+        residual=x
+        x=torch.nn.functional.relu(x)
+        x=self.batchnorm(x)
+        for layer in self.conv3x3:
+            x=layer(x)
+        x+=residual
+        return x
+
 
 ####################### ResNet architectures #######################
 class ResNetParallel(base_model.BaseModel):
@@ -141,6 +160,21 @@ class ResNetChoke(base_model.BaseModel):
         self.network=nn.ModuleList([])
         for aa in range(config["residual_blocks"]):
             self.network.append(ResidualBlock(2,config["conv_filters"],2,3))
+        
+    def forward(self,x):
+        for module in self.network:
+            x=module(x)
+        return x
+
+
+class ResNetLinear(BaseModel):
+    """ Resnet where each residual block is collapsed down to two channels, one for upper and one for
+        lower layer """
+    def __init__(self,config,model_beta=None,residual=False):
+        super().__init__(config,model_beta,residual)
+        self.network=nn.ModuleList([])
+        for aa in range(config["residual_blocks"]):
+            self.network.append(LinearResidualBlock(2,config["conv_filters"],2,3))
         
     def forward(self,x):
         for module in self.network:
