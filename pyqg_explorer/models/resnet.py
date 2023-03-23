ca8067f8548bcf086849e10a78e780da5cb9e748
Chris-Pedersen <c.pedersen@nyu.edu>
2023-03-23 13:37:02 -0400

    Tidy up resnet code, remove redundant objects. Default architecture is now the pre-activation structure

--- pyqg_explorer/models/resnet.py
+++ pyqg_explorer/models/resnet.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import pyqg_explorer.models.base_model as base_model
 
 
-####################### Various residual block objects #######################
+####################### Various residual block components #######################
 def GetConvBlock(in_channels,intermediate_channels,out_channels,kernel_size=3,dropout=0.0):
     conv1=nn.Conv2d(in_channels,intermediate_channels,kernel_size,padding="same",padding_mode="circular")
     dropout=nn.Dropout2d(dropout)
@@ -15,32 +15,14 @@ def GetConvBlock(in_channels,intermediate_channels,out_channels,kernel_size=3,dr
     block.append(conv2)
     return nn.Sequential(*block)
 
-
-class ResidualBlock(nn.Module):
-    """ Stacks 2 convolutional layers with a residual connection, forming a single
-        residual block """
-    def __init__(self,in_channels,intermediate_channels,out_channels,kernel_size):
-        super().__init__()
-        self.conv3x3=nn.ModuleList([*GetConvBlock(in_channels,intermediate_channels,out_channels,kernel_size)])
-        self.batchnorm=nn.BatchNorm2d(in_channels)
-        
-    def forward(self,x):
-        residual=x
-        for layer in self.conv3x3:
-            x=layer(x)
-        x+=residual
-        x=torch.nn.functional.relu(x)
-        x=self.batchnorm(x)
-        return x
-
     
-class NarrowResBlock(nn.Module):
-    """ Stacks 2 convolutional layers with a residual connection, forming a single
-        residual block """
-    def __init__(self,in_channels,intermediate_channels,out_channels,kernel_size):
+class WideResBlock(nn.Module):
+    """ Map skip connections to N>2 intermediate layers, with the upper layer connected to the first N/2, 
+    and the lower layer connected to the second N/2. For use in the WideResidualNetwork"""
+    def __init__(self,in_channels,intermediate_channels,out_channels,kernel_size,dropout=0.0):
         super().__init__()
         self.cut_idx=int(out_channels/2)
-        self.conv3x3=nn.ModuleList([*GetConvBlock(in_channels,intermediate_channels,out_channels,kernel_size)])
+        self.conv3x3=nn.ModuleList([*GetConvBlock(in_channels,intermediate_channels,out_channels,kernel_size,dropout)])
         self.batchnorm=nn.BatchNorm2d(in_channels)
         
     def forward(self,x):
@@ -50,18 +32,18 @@ class NarrowResBlock(nn.Module):
         for layer in self.conv3x3:
             x=layer(x)
         ## "Narrow" residual connection
-        x=torch.cat(((x[:,:self.cut_idx,:,:]+residual[:,0,:,:].unsqueeze(1)),x[:,self.cut_idx:,:,:]+residual[:,0,:,:].unsqueeze(1)),dim=1)
+        x=torch.cat(((x[:,:self.cut_idx,:,:]+residual[:,0,:,:].unsqueeze(1)),x[:,self.cut_idx:,:,:]+residual[:,1,:,:].unsqueeze(1)),dim=1)
         return x
 
 
-class LinearResidualBlock(nn.Module):
+class ResidualBlock(nn.Module):
     """ Stacks 2 convolutional layers with a residual connection, forming a single
         residual block. Here the nonlinearity and batchnorm is applied *before*
-        the residual connection """
-    def __init__(self,in_channels,intermediate_channels,out_channels,kernel_size):
+        the residual connection, following 1603.05027 and 1605.07146 """
+    def __init__(self,in_channels,intermediate_channels,out_channels,kernel_size,dropout=0.0):
         super().__init__()
         self.batchnorm=nn.BatchNorm2d(in_channels)
-        self.conv3x3=nn.ModuleList([*GetConvBlock(in_channels,intermediate_channels,out_channels,kernel_size)])
+        self.conv3x3=nn.ModuleList([*GetConvBlock(in_channels,intermediate_channels,out_channels,kernel_size,dropout)])
         
     def forward(self,x):
         residual=x
@@ -74,7 +56,7 @@ class LinearResidualBlock(nn.Module):
 
 
 ####################### ResNet architectures #######################
-class ResNetChoke(base_model.BaseModel):
+class ResNet(base_model.BaseModel):
     """ Resnet where each residual block is collapsed down to two channels, one for upper and one for
         lower layer """
     def __init__(self,config,model_beta=None,residual=False):
@@ -89,16 +71,21 @@ class ResNetChoke(base_model.BaseModel):
         return x
 
 
-class ResNetLinear(base_model.BaseModel):
-    """ Resnet where each residual block is collapsed down to two channels, one for upper and one for
-        lower layer """
+class WideResNet(base_model.BaseModel):
+    """ Resnet where we have a larger number of intermediate convolutional layers - using skip connections
+        from the upper layer to the first half of the intermediate fields, and from the lower layer to the second
+        half. This mapping is performed in the torch.cat line of the WideResidualBlock.
+
+
+        NB Not sure if we actually want to do this, as collapsing these layers back down into a single upper
+        and lower layer prediction would struggle to map the identity. So setting this as NotImplemented for now
+        and will delete it down the line if we don't come back to it."""
+
     def __init__(self,config,model_beta=None,residual=False):
         super().__init__(config,model_beta,residual)
         self.network=nn.ModuleList([])
         for aa in range(config["residual_blocks"]):
-            self.network.append(LinearResidualBlock(2,config["conv_filters"],2,3))
+            self.network.append(WideResidualBlock(2,config["conv_filters"],2,3),config["dropout"])
         
     def forward(self,x):
-        for module in self.network:
-            x=module(x)
-        return x
+        raise NotImplementedError
