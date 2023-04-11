468606ad90ba11c784712142ec66b997a635f03c
Chris-Pedersen <c.pedersen@nyu.edu>
2023-04-11 16:22:32 -0400

    Add option to cache subgrid forcing field at each call. We can then request this inside a training loop. Useful for constructing an emulator dataset for a low-res parameterised sim, where we want to store the subgrid forcing at each timestep

--- pyqg_explorer/models/parameterizations.py
+++ pyqg_explorer/models/parameterizations.py
@@ -6,11 +6,16 @@ import numpy as np
 class Parameterization(pyqg.QParameterization):
     """ pyqg subgrid parameterisation for the potential vorticity"""
     
-    def __init__(self,model,normalise=True):
+    def __init__(self,model,normalise=True,cache_forcing=False):
         """ Initialise with a list of torch models, one for each layer """
         self.model=model
         self.model.eval() ## Ensure we are in eval
         self.normalise=normalise
+        self.cache_forcing=cache_forcing
+        self.cached_forcing=None
+
+    def get_cached_forcing(self):
+        return self.cached_forcing
 
     def __call__(self, m):
         """ 
@@ -20,12 +25,14 @@ class Parameterization(pyqg.QParameterization):
                 - forcing: a numpy array of shape (nz, nx, ny) with the subgrid
                            forcing values """
 
-
         s=self.model.pred(m.q)
 
         if self.normalise:
             means=np.mean(s,axis=(1,2))
             s[0]=s[0]-means[0]
             s[1]=s[1]-means[1]
+
+        if self.cache_forcing:
+            self.cached_forcing=s
 
         return s
