04ba663109ee7c15f549acefdd32b218f04790c7
Chris-Pedersen <c.pedersen@nyu.edu>
2023-04-11 16:24:17 -0400

    Update model save method, and online pred calls. Might want to abstract this to some base class in the future, as currently no way to load and use resnets as parameterizations.

--- pyqg_explorer/models/fcnn.py
+++ pyqg_explorer/models/fcnn.py
@@ -1,6 +1,8 @@
 
 import torch
 import torch.nn as nn
+import numpy as np
+import pyqg_explorer.util.transforms as transforms
 
 ## From Andrew/Pavel's code, function to create a CNN block
 def make_block(in_channels: int, out_channels: int, kernel_size: int, 
@@ -59,3 +61,40 @@ class FCNN(nn.Module):
     def forward(self, x):
         x = self.conv(x)
         return x
+
+    def save_model(self):
+        """ Save the model config, and optimised weights and biases. We create a dictionary
+        to hold these two sub-dictionaries, and save it as a pickle file """
+        if self.config["save_path"] is None:
+            print("No save path provided, not saving")
+            return
+        save_dict={}
+        save_dict["state_dict"]=self.state_dict() ## Dict containing optimised weights and biases
+        save_dict["config"]=self.config           ## Dict containing config for the dataset and model
+        save_string=os.path.join(self.config["save_path"],self.config["save_name"])
+        with open(save_string, 'wb') as handle:
+            pickle.dump(save_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        print("Model saved as %s" % save_string)
+        return
+
+    def pred(self, x):
+        """ Method to call when receiving un-normalised data, when implemented as a pyqg
+            parameterisation. Expects a 3D numpy array """
+
+        x=torch.tensor(x).float()
+        ## Map from physical to normalised space using the factors used to train the network
+        ## Normalise each field individually, then cat arrays back to shape appropriate for a torch model
+        x_upper = transforms.normalise_field(x[0],self.config["q_mean_upper"],self.config["q_std_upper"])
+        x_lower = transforms.normalise_field(x[1],self.config["q_mean_lower"],self.config["q_std_lower"])
+        x = torch.stack((x_upper,x_lower),dim=0).unsqueeze(0)
+
+        ## Pass the normalised fields through our network
+        x = self(x)
+
+        ## Map back from normalised space to physical units
+        s_upper=transforms.denormalise_field(x[:,0,:,:],self.config["s_mean_upper"],self.config["s_std_upper"])
+        s_lower=transforms.denormalise_field(x[:,1,:,:],self.config["s_mean_lower"],self.config["s_std_lower"])
+
+        ## Reshape to match pyqg dimensions, and cast to numpy array
+        s=torch.cat((s_upper,s_lower)).detach().numpy().astype(np.double)
+        return s
