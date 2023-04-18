c630d2e72262fa4b6c883a99acc99c752289ce40
Chris-Pedersen <c.pedersen@nyu.edu>
2023-04-18 17:53:25 -0400

    Add fcnn from Srinivasan 2023

--- pyqg_explorer/models/fcnn.py
+++ pyqg_explorer/models/fcnn.py
@@ -16,6 +16,8 @@ def make_block(in_channels: int, out_channels: int, kernel_size: int,
     block = [conv]
     if ReLU == 'ReLU':
         block.append(nn.ReLU())
+    if ReLU == 'SiLU':
+        block.append(nn.SiLU())
     elif ReLU == 'LeakyReLU':
         block.append(nn.LeakyReLU(0.2))
     elif ReLU == 'False':
@@ -25,6 +27,7 @@ def make_block(in_channels: int, out_channels: int, kernel_size: int,
     if batch_norm:
         block.append(nn.BatchNorm2d(out_channels))
     return block
+
 
 
 class FCNN(nn.Module):
@@ -56,6 +59,65 @@ class FCNN(nn.Module):
                 blocks.extend(make_block(32,32,3,self.config["activation"]))
             ## Output layer
             blocks.extend(make_block(32,self.config["output_channels"],3,'False',False))
+        self.conv = nn.Sequential(*blocks)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+    def save_model(self):
+        """ Save the model config, and optimised weights and biases. We create a dictionary
+        to hold these two sub-dictionaries, and save it as a pickle file """
+        if self.config["save_path"] is None:
+            print("No save path provided, not saving")
+            return
+        save_dict={}
+        save_dict["state_dict"]=self.state_dict() ## Dict containing optimised weights and biases
+        save_dict["config"]=self.config           ## Dict containing config for the dataset and model
+        save_string=os.path.join(self.config["save_path"],self.config["save_name"])
+        with open(save_string, 'wb') as handle:
+            pickle.dump(save_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        print("Model saved as %s" % save_string)
+        return
+
+    def pred(self, x):
+        """ Method to call when receiving un-normalised data, when implemented as a pyqg
+            parameterisation. Expects a 3D numpy array """
+
+        x=torch.tensor(x).float()
+        ## Map from physical to normalised space using the factors used to train the network
+        ## Normalise each field individually, then cat arrays back to shape appropriate for a torch model
+        x_upper = transforms.normalise_field(x[0],self.config["q_mean_upper"],self.config["q_std_upper"])
+        x_lower = transforms.normalise_field(x[1],self.config["q_mean_lower"],self.config["q_std_lower"])
+        x = torch.stack((x_upper,x_lower),dim=0).unsqueeze(0)
+
+        ## Pass the normalised fields through our network
+        x = self(x)
+
+        ## Map back from normalised space to physical units
+        s_upper=transforms.denormalise_field(x[:,0,:,:],self.config["s_mean_upper"],self.config["s_std_upper"])
+        s_lower=transforms.denormalise_field(x[:,1,:,:],self.config["s_mean_lower"],self.config["s_std_lower"])
+
+        ## Reshape to match pyqg dimensions, and cast to numpy array
+        s=torch.cat((s_upper,s_lower)).detach().numpy().astype(np.double)
+        return s
+
+
+class Sri_FCNN(nn.Module):
+    def __init__(self,config):
+        '''
+        Packs sequence of n_conv=config["conv_layers"] convolutional layers in a list.
+        First layer has config["input_channels"] input channels, and last layer has
+        config["output_channels"] output channels
+        '''
+        super().__init__()
+        self.config=config
+
+        blocks = []
+        ## If the conv_layers key is missing, we are running
+        ## with an 8 layer CNN
+        blocks.extend(make_block(self.config["input_channels"],self.config["n_filters"],5,self.config["activation"],False)) #1
+        blocks.extend(make_block(self.config["n_filters"],2,5,'False',False)) 
         self.conv = nn.Sequential(*blocks)
 
     def forward(self, x):
