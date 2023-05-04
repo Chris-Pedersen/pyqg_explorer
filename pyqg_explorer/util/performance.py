dbf02488bd6b35c039d7d3c82336f0247c54ca41
Chris-Pedersen <c.pedersen@nyu.edu>
2023-05-04 17:05:52 -0400

    Add emulator performance module

--- pyqg_explorer/util/performance.py
+++ pyqg_explorer/util/performance.py
@@ -12,8 +12,120 @@ import cmocean
 
 class EmulatorPerformance():
     """ Object to store performance tests relevant to neural emulators """
-    def __init__(self):
-        return
+    def __init__(self,network,valid_loader,threshold):
+        """ network:  Torch model we want to test. Assuming this is a model for the subgrid forcing
+            valid_loader: torch dataloader with the validation set
+                          NB we are assuming this is an EmulatorForcingDataset, where the
+                          true subgrid forcing is in the x[:,2:4,:,:] indices
+            threshold: The number of data samples at which to stop iterating dataloader. We take a subsample
+                       in order to avoid running out of memory, and have demonstrated that a few thousand samples
+                       is enough to estimate offline metrics
+                       
+                       
+            TO ADD: an assertion clause to make sure the random seed in the model and dataset are the same, to ensure we are
+                    predicting on unseen data """
+        
+        self.network=network
+        self.x_np=[]
+        self.y_true=[] ## Let's store the i+1 field
+        self.y_pred=[] ## for both true and predicted
+        
+        count=0
+        ## Cache x, true y and predicted y values that we will use to guage offline performance
+        for data in valid_loader:
+            x=data[0]
+            y=data[1]
+            count+=x.shape[0]
+            self.x_np.append(x)
+            self.y_true.append(y)
+            self.y_pred.append(self.network(x)+x[:,0:2,:,:]) ## y pred should now be the same quantity, q_i+dt
+            if count>threshold:
+                break
+        self.x_np=torch.vstack(self.x_np).detach().numpy()
+        self.y_true=torch.vstack(self.y_true).detach().numpy()
+        self.y_pred=torch.vstack(self.y_pred).detach().numpy()
+
+        ## Save R2, since we basically get it for free at this point
+        self.r2_upper=r2_score(self.y_true[:,0,:,:].flatten(),self.y_pred[:,0,:,:].flatten())
+        self.r2_lower=r2_score(self.y_true[:,1,:,:].flatten(),self.y_pred[:,1,:,:].flatten())
+        
+    def get_distribution_2d(self):
+        """ Plot histograms of the true and predicted subgrid forcing """
+        fig, axs = plt.subplots(1, 2,figsize=(11,4))
+        axs[0].set_title(r"Upper layer: $R^2$=%.5f" % self.r2_upper)
+        axs[1].set_title(r"Lower layer: $R^2$=%.5f" % self.r2_lower)
+        line=np.linspace(-4,4,100)
+        axs[0].plot(line,line,linestyle="dashed",color="gray",alpha=0.5)
+        ax=axs[0].hist2d(self.y_true[:,0,:,:].flatten()-self.x_np[:,0,:,:].flatten(),self.y_pred[:,0,:,:].flatten()-self.x_np[:,0,:,:].flatten(),bins=100,range=[[-0.1,0.1],[-0.1,0.1]],cmap='RdPu');
+        fig.colorbar(ax[3], ax=axs[0])
+        axs[1].plot(line,line,linestyle="dashed",color="gray",alpha=0.5)
+        ax=axs[1].hist2d(self.y_true[:,1,:,:].flatten()-self.x_np[:,1,:,:].flatten(),self.y_pred[:,1,:,:].flatten()-self.x_np[:,1,:,:].flatten(),bins=100,range=[[-0.1,0.1],[-0.1,0.1]],cmap='RdPu');
+        fig.colorbar(ax[3], ax=axs[1])
+        return fig
+        
+    def get_fields(self,map_index=None):
+        """ For a single data sample, plot the input field, target output, predicted output, and diff between the two """
+        
+        ## Chose random index unless one is provided
+        if map_index is None:
+            map_index=np.random.randint(len(self.x_np)-1)
+    
+        fig, axs = plt.subplots(2, 4,figsize=(15,6))
+        image=self.x_np[map_index][0]
+        limit=np.max(np.abs(image))
+        ax=axs[0][0].imshow(image, cmap=cmocean.cm.balance,vmin=-limit,vmax=limit,interpolation='none')
+        fig.colorbar(ax, ax=axs[0][0])
+        axs[0][0].set_xticks([]); axs[0][0].set_yticks([])
+        axs[0][0].set_title("q_i")
+
+        image=self.y_true[map_index][0]-self.x_np[map_index][0]
+        limit=np.max(np.abs(image))
+        ax=axs[0][1].imshow(image, cmap=cmocean.cm.balance,vmin=-limit,vmax=limit, interpolation='none')
+        fig.colorbar(ax, ax=axs[0][1])
+        axs[0][1].set_xticks([]); axs[0][1].set_yticks([])
+        axs[0][1].set_title("true res")
+
+        image=self.y_pred[map_index][0]-self.x_np[map_index][0]
+        limit=np.max(np.abs(image))
+        ax=axs[0][2].imshow(image, cmap=cmocean.cm.balance,vmin=-limit,vmax=limit, interpolation='none')
+        fig.colorbar(ax, ax=axs[0][2])
+        axs[0][2].set_xticks([]); axs[0][2].set_yticks([])
+        axs[0][2].set_title("pred res")
+
+        image=self.y_true[map_index][0]-self.y_pred[map_index][0]
+        limit=np.max(np.abs(image))
+        ax=axs[0][3].imshow(image, cmap=cmocean.cm.balance,vmin=-limit,vmax=limit, interpolation='none')
+        fig.colorbar(ax, ax=axs[0][3])
+        axs[0][3].set_xticks([]); axs[0][3].set_yticks([])
+        axs[0][3].set_title("diff")
+        fig.tight_layout()
+
+        image=self.x_np[map_index][1]
+        limit=np.max(np.abs(image))
+        ax=axs[1][0].imshow(image, cmap=cmocean.cm.balance,vmin=-limit,vmax=limit, interpolation='none')
+        fig.colorbar(ax, ax=axs[1][0])
+        axs[1][0].set_xticks([]); axs[1][0].set_yticks([])
+
+        image=self.y_true[map_index][1]-self.x_np[map_index][1]
+        limit=np.max(np.abs(image))
+        ax=axs[1][1].imshow(image, cmap=cmocean.cm.balance,vmin=-limit,vmax=limit, interpolation='none')
+        fig.colorbar(ax, ax=axs[1][1])
+        axs[1][1].set_xticks([]); axs[1][1].set_yticks([])
+
+        image=self.y_pred[map_index][1]-self.x_np[map_index][1]
+        limit=np.max(np.abs(image))
+        ax=axs[1][2].imshow(image, cmap=cmocean.cm.balance,vmin=-limit,vmax=limit, interpolation='none')
+        fig.colorbar(ax, ax=axs[1][2])
+        axs[1][2].set_xticks([]); axs[1][2].set_yticks([])
+
+        image=self.y_true[map_index][1]-self.y_pred[map_index][1]
+        limit=np.max(np.abs(image))
+        ax=axs[1][3].imshow(image, cmap=cmocean.cm.balance,vmin=-limit,vmax=limit, interpolation='none')
+        fig.colorbar(ax, ax=axs[1][3])
+        axs[1][3].set_xticks([]); axs[1][3].set_yticks([])
+        fig.tight_layout()
+        
+        return fig
 
 
 class ParameterizationPerformance():
@@ -115,6 +227,7 @@ class ParameterizationPerformance():
         return fig
         
     def get_fields(self,map_index=None):
+        """ For a single data sample, plot the input field, target output, predicted output, and diff between the two """
         
         ## Chose random index unless one is provided
         if map_index is None:
