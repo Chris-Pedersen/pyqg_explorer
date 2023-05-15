c2a23e7ca296b1dfa96d96e55d79c7c5ec01307e
Chris-Pedersen <c.pedersen@nyu.edu>
2023-05-15 13:47:51 -0400

    Remove save and pred methods from BaseRegressionSystem - these are part of the ML model

--- pyqg_explorer/systems/regression_systems.py
+++ pyqg_explorer/systems/regression_systems.py
@@ -59,43 +59,6 @@ class BaseRegSytem(LightningModule):
 
     def validation_step(self, batch, batch_idx):
         return self.step(batch,"valid")
-    
-    def save_model(self):
-        """ Save the model config, and optimised weights and biases. We create a dictionary
-        to hold these two sub-dictionaries, and save it as a pickle file """
-        if self.config["save_path"] is None:
-            print("No save path provided, not saving")
-            return
-        save_dict={}
-        save_dict["state_dict"]=self.state_dict() ## Dict containing optimised weights and biases
-        save_dict["config"]=self.config           ## Dict containing config for the dataset and model
-        save_string=os.path.join(self.config["save_path"],self.config["save_name"])
-        with open(save_string, 'wb') as handle:
-            pickle.dump(save_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        print("Model saved as %s" % save_string)
-        return
-
-    def pred(self, x):
-        """ Method to call when receiving un-normalised data, when implemented as a pyqg
-            parameterisation. Expects a 3D numpy array """
-
-        x=torch.tensor(x).float()
-        ## Map from physical to normalised space using the factors used to train the network
-        ## Normalise each field individually, then cat arrays back to shape appropriate for a torch model
-        x_upper = transforms.normalise_field(x[0],self.config["q_mean_upper"],self.config["q_std_upper"])
-        x_lower = transforms.normalise_field(x[1],self.config["q_mean_lower"],self.config["q_std_lower"])
-        x = torch.stack((x_upper,x_lower),dim=0).unsqueeze(0)
-
-        ## Pass the normalised fields through our network
-        x = self(x)
-
-        ## Map back from normalised space to physical units
-        s_upper=transforms.denormalise_field(x[:,0,:,:],self.config["s_mean_upper"],self.config["s_std_upper"])
-        s_lower=transforms.denormalise_field(x[:,1,:,:],self.config["s_mean_lower"],self.config["s_std_lower"])
-
-        ## Reshape to match pyqg dimensions, and cast to numpy array
-        s=torch.cat((s_upper,s_lower)).detach().numpy().astype(np.double)
-        return s
 
 
 class RegressionSystem(BaseRegSytem):
