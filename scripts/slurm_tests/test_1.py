9f7b26571a348fb24d42cfaae326f2b73a814083
Chris-Pedersen <c.pedersen@nyu.edu>
2023-08-19 16:18:46 -0400

    Didn't need this line - was meant to just be in the submission script

--- scripts/slurm_tests/test_1.py
+++ scripts/slurm_tests/test_1.py
@@ -2,5 +2,4 @@
 import os
 
 print("attempting to run other script")
-os.system("source /scratch/work/public/singularity/greene-ib-slurm-bind.sh")
 os.system("sbatch /home/cp3759/Testbed/slurm_tests/submit_second.sh")
