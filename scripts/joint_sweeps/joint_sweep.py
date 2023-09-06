55aec09f33d63e3855fa091f872888a7ab003d92
Chris-Pedersen <c.pedersen@nyu.edu>
2023-09-06 15:36:09 -0400

    Update script to run joint optimisation (for a single step into future)

--- scripts/joint_sweeps/joint_sweep.py
+++ scripts/joint_sweeps/joint_sweep.py
@@ -1,5 +1,7 @@
 import wandb
 import sys
+import copy
+import os
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 import pytorch_lightning as pl
@@ -12,25 +14,48 @@ import pyqg_explorer.util.performance as performance
 import pyqg_explorer.util.misc as misc
 import pyqg_explorer.dataset.forcing_dataset as forcing_dataset
 
+import pickle
+import os
+import functools
+import torch
+import torch.nn as nn
+import numpy as np
+from pytorch_lightning import LightningModule
+from pytorch_lightning.loops import FitLoop
+import pyqg_explorer.util.transforms as transforms
 
-config=reg_sys.config
-time_hor=int(sys.argv[1])
-config["drop_spin_up"]=True
 
-def train(time_horizon,beta_loss):
+def train(beta_loss):
+    config=copy.deepcopy(reg_sys.config)
+    time_hor=2
+    config["drop_spin_up"]=True
+    config["scheduler"]=False
+    config["beta_gradients"]=False
     config["beta_loss"]=beta_loss
-    config["time_horizon"]=time_horizon
+    config["time_horizon"]=2
+    config["scheduler"]=True
     ## Emulator model used for forward prediction
-    config["model_string"]="/scratch/cp3759/pyqg_data/models/emulator/fcnn_residuals/fcnnr_4_%d_step_all_May.p" % config["time_horizon"]
-    model_beta=misc.load_model(config["model_string"])
-    config["epochs"]=100
+
+    config["epochs"]=120
     config["subgrid_models"]=["HRC"]
     config["theta_loss"]=1
-    config["save_path"]="/scratch/cp3759/pyqg_data/models/joint_May_nospin"
-    config["save_name"]="joint_beta%d_time%d.p" % (config["beta_loss"],config["time_horizon"])
 
+    ## Remove save_path if it exists..
+    try:
+        config.pop("save_path")
+    except:
+        pass
+
+    config["model_string"]="/scratch/cp3759/pyqg_data/wandb_runs/wandb/run-20230830_234316-gnduwhx5/files/model_weights.pt" ## dt=2, trained ove 4 decaying rollouts
+    model_beta=misc.load_model(config["model_string"])
+    model_beta.requires_grad_(False)
     dataset=forcing_dataset.EmulatorForcingDataset('/scratch/cp3759/pyqg_data/sims/%d_step_forcing/' % config["time_horizon"],config["subgrid_models"],
                                 channels=4,seed=config["seed"],subsample=config["subsample"],drop_spin_up=config["drop_spin_up"])
+
+    ## Fix emulator network gradients
+    if config["beta_gradients"]==False:
+        model_beta.requires_grad_(False)
+
 
     train_loader = DataLoader(
         dataset,
@@ -56,12 +81,16 @@ def train(time_horizon,beta_loss):
     config["training_fields"]=len(dataset.train_idx)
     config["validation_fields"]=len(dataset.valid_idx)
 
+    wandb.init(project="joint_dt2", entity="m2lines",config=config,dir="/scratch/cp3759/pyqg_data/wandb_runs")
+    ## Have to update both the wandb config and the config dict that is passed to the CNN
+    wandb.config["save_path"]=wandb.run.dir
+    config["save_path"]=wandb.run.dir
+    config["wandb_url"]=wandb.run.get_url()
+
     model=fcnn.FCNN(config)
 
     system=reg_sys.JointRegressionSystem(model,config,model_beta)
-    system.network_beta.requires_grad=False
 
-    wandb.init(project="joint_opt_sweep", entity="m2lines",config=config,dir="/scratch/cp3759/pyqg_data/wandb_runs")
     wandb.config["theta learnable parameters"]=sum(p.numel() for p in model.parameters())
     wandb.watch(model, log_freq=1)
 
@@ -96,11 +125,19 @@ def train(time_horizon,beta_loss):
     figure_online=wandb.Image(online_fig)
     wandb.log({"Online test": figure_online})
 
+    subgrid_fig=perf.subgrid_energy()
+    figure_subgrid=wandb.Image(subgrid_fig)
+    wandb.log({"Subgrid energy": figure_subgrid})
+
     model.save_model()
+
+    ## Run KE sims
+    os.system("bash /home/cp3759/Projects/pyqg_explorer/scripts/KE_datasets/submit_ensembles.sh --wandb_dir %s --wandb_url %s" % (config["save_path"],config["wandb_url"]))
+
     wandb.finish()
 
-loss_betas=[1,10,100,1000]
+loss_betas=[1,10,100,1000,10000,100000]
 for beta_loss in loss_betas:
-    train(time_hor,beta_loss)
+    train(beta_loss)
 
 print("Finito")
