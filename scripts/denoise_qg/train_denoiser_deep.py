80a1649ab5cdae11f649c5b4f8ce73ed03037ddd
Chris-Pedersen <c.pedersen@nyu.edu>
2024-02-17 22:29:10 -0500

    Update denoise scripts - moved quality plots to module library, stopped logging every n steps, removed clip config arg, added variable valid batch size, added spectra plot at the end of each epoch showing spectra for denoised vields from a full validation batch. Also set up config to work with body eddy and jet

--- scripts/denoise_qg/train_denoiser_deep.py
+++ scripts/denoise_qg/train_denoiser_deep.py
@@ -1,37 +1,30 @@
 import argparse
 import wandb
-from torch.utils.data import DataLoader
-from torch.utils.data.sampler import SubsetRandomSampler
-
-import pyqg_explorer.systems.regression_systems as reg_sys
-import pyqg_explorer.models.diffusion as diffusion
-import pyqg_explorer.models.fcnn as fcnn
-import pyqg_explorer.models.unet as unet
-import pyqg_explorer.dataset.forcing_dataset as forcing_dataset
+import math
+import numpy as np
 import matplotlib.pyplot as plt
 import cmocean
 
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data.sampler import SubsetRandomSampler
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import OneCycleLR
-import math
-import numpy as np
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
+
+import pyqg_explorer.performance.diffusion_performance as diff_performance
+import pyqg_explorer.systems.regression_systems as reg_sys
+import pyqg_explorer.models.diffusion as diffusion
+import pyqg_explorer.models.fcnn as fcnn
+import pyqg_explorer.models.unet as unet
+import pyqg_explorer.dataset.forcing_dataset as forcing_dataset
 
 parser = argparse.ArgumentParser(description="Diffusion model denoiser")
 parser.add_argument(
     "--base-dim", required=True, type=float, help="base dim of Unet"
 )
-parser.add_argument(
-    "--denoise-time",
-    required=True,
-    type=float,
-    default=200,
-    help="denoise level",
+parser.add_argument("--denoise-time",required=True,type=float,default=200,help="denoise level",
 )
 parser.add_argument("--subsample",type=int,
     help="subsample training data",
@@ -42,28 +35,29 @@ config=reg_sys.config
 ## Stuff we are varying
 config["subsample"]=int(args.subsample)
 config["base_dim"]=int(args.base_dim)
-config["dim_mults"]=[2,4,8]
-config["noise_sampling_coeff"]=0.35
 config["denoise_time"]=int(args.denoise_time)
 
 ## Stuff we are not varying (for now)
+config["noise_sampling_coeff"]=0.35
+config["dim_mults"]=[2,4,8]
 config["lr"]=0.001
 config["batch_size"]=128
 config["epochs"]=100
 config["timesteps"]=1000
 config["model_ema_steps"]=0
 config["model_ema_decay"]=0.995
-config["log_freq"]=50
-config["no_clip"]=True # set to normal sampling method without clip x_0 which could yield unstable samples
 config["time_embedding_dim"]=256
 config["input_channels"]=2
 config["output_channels"]=2
 config["image_size"]=64
 config["save_name"]="model_weights.pt"
+config["eddy"]=False
+config["valid_batch_size"]=20
 
-snapshot_dataset=forcing_dataset.OfflineDataset("/scratch/cp3759/pyqg_data/sims/torchqg_sims/0_step/all_jet.nc",seed=config["seed"],subsample=config["subsample"],drop_spin_up=config["drop_spin_up"])
+snapshot_dataset=forcing_dataset.OfflineDataset("/scratch/cp3759/pyqg_data/sims/torchqg_sims/0_step/all_jet.nc",
+                            seed=config["seed"],subsample=config["subsample"],drop_spin_up=config["drop_spin_up"])
 
-## Need to save renormalisation factors for when the CNN is plugged into pyqg
+## Save renormalisation factors for mapping NN predictions to and from physical units
 config["q_mean_upper"]=snapshot_dataset.q_mean_upper
 config["q_mean_lower"]=snapshot_dataset.q_mean_lower
 config["q_std_upper"]=snapshot_dataset.q_std_upper
@@ -84,14 +78,13 @@ train_loader = DataLoader(
 valid_loader = DataLoader(
     snapshot_dataset,
     num_workers=10,
-    batch_size=20,
+    batch_size=config["valid_batch_size"],
     sampler=SubsetRandomSampler(snapshot_dataset.valid_idx),
     drop_last=True
 )
-
 device="cuda"
 
-wandb.init(project="denoise_qg",entity="m2lines",config=config,dir="/scratch/cp3759/pyqg_data/wandb_runs")
+wandb.init(project="denoise_dev",entity="m2lines",config=config,dir="/scratch/cp3759/pyqg_data/wandb_runs")
 
 ## Make sure save path, train set size, wandb url are passed to config before model is initialised!
 ## otherwise these important things aren't part of the model config property
@@ -123,25 +116,16 @@ for i in range(1,config["epochs"]+1):
         optimizer.step()
         optimizer.zero_grad()
         scheduler.step()
-        #if global_steps%config["model_ema_steps"]==0:
-        #    model_ema.update_parameters(model)
         global_steps+=1
-        if j%config["log_freq"]==0:
-            log_dic={}
-            log_dic["training_loss"]=(loss.detach().cpu().item()/(len(image)))
-            log_dic["lr"]=scheduler.get_last_lr()[0]
-            wandb.log(log_dic)
-            #print("Epoch[{}/{}],Step[{}/{}],loss:{:.5f},lr:{:.5f}".format(i+1,epochs,j,len(train_loader),
-            #                                                    loss.detach().cpu().item(),scheduler.get_last_lr()[0]))
     
     ## Push loss values for each epoch to wandb
     log_dic={}
     log_dic["epoch"]=i
     log_dic["training_loss"]=(loss.detach().cpu().item()/(len(image)))
     
-    
     model.eval()
-    
+    ## From a validation set, add noise to a set of heldout images
+    ## Then denoise them, and run some quality metrics on the reconstructions
     valid_imgs=next(iter(valid_loader))
     valid_imgs=valid_imgs[0].to(device)
     noise=torch.randn_like(valid_imgs).to(device)
@@ -149,56 +133,25 @@ for i in range(1,config["epochs"]+1):
     noised=model._forward_diffusion(valid_imgs,t,noise)
     denoised=model.denoising(noised,config["denoise_time"])
     
-    plt.figure(figsize=(8,4))
-    plt.suptitle("Denoised fields at epoch=%d" % i)
-    
+    ## Get losses of noise level and denoised images
     noise_loss=loss_fn(valid_imgs,noised)
     valid_denoise_loss=loss_fn(valid_imgs,denoised)
-
-    valid_idx=np.random.randint(len(valid_imgs))
-    fig_denoise=plt.subplot(2,3,1)
-    plt.title("Original field")
-    plt.imshow(valid_imgs[valid_idx][0].cpu().numpy(),cmap=cmocean.cm.balance)
-    plt.xticks([], [])
-    plt.yticks([], [])
-    plt.colorbar()
-    plt.subplot(2,3,4)
-    plt.xticks([], [])
-    plt.yticks([], [])
-    plt.imshow(valid_imgs[valid_idx][1].cpu().numpy(),cmap=cmocean.cm.balance)
-    plt.colorbar()
-
-    plt.subplot(2,3,2)
-    plt.xticks([], [])
-    plt.yticks([], [])
-    plt.title("Noised field: %.2f" % noise_loss)
-    plt.imshow(noised[valid_idx][0].cpu().numpy(),cmap=cmocean.cm.balance)
-    plt.colorbar()
-    plt.subplot(2,3,5)
-    plt.xticks([], [])
-    plt.yticks([], [])
-    plt.imshow(noised[valid_idx][1].cpu().numpy(),cmap=cmocean.cm.balance)
-    plt.colorbar()
-
-    plt.subplot(2,3,3)
-    plt.xticks([], [])
-    plt.yticks([], [])
-    plt.title("Denoised field: %.2f" % valid_denoise_loss)
-    plt.imshow(denoised[valid_idx][0].cpu().numpy(),cmap=cmocean.cm.balance)
-    plt.colorbar()
-    plt.subplot(2,3,6)
-    plt.xticks([], [])
-    plt.yticks([], [])
-    plt.imshow(denoised[valid_idx][1].cpu().numpy(),cmap=cmocean.cm.balance)
-    plt.colorbar()
-
-    plt.tight_layout()
-    wandb.log({"Denoise":fig_denoise})
-    plt.close()
+    
+    fig_denoise=diff_performance.plot_fields(valid_imgs,noised,denoised,noise_loss,valid_denoise_loss,i)
+    denoise_fig=wandb.Image(fig_denoise)
+    wandb.log({"Denoise":denoise_fig})
+    
+    ## Pass clean and denoised fields to simulation objects, such that we can
+    ## calculate diagnostics
+    clean_sims,denoised_sims=diff_performance.field_to_sims(valid_imgs,denoised,config)
+    fig_spectra=diff_performance.spectral_diagnostics(clean_sims,denoised_sims,i,config["eddy"])
+    spectra_fig=wandb.Image(fig_spectra)
+    wandb.log({"Spectra":spectra_fig})
     
     log_dic["denoise_loss"]=(valid_denoise_loss.detach().cpu().item()/(len(valid_imgs)))
     log_dic["noise_loss"]=(noise_loss.detach().cpu().item()/(len(valid_imgs)))
     wandb.log(log_dic)
+    plt.close()
 
 model.model.save_model()
 wandb.finish()
