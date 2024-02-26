ded852d6006d1ce69a30bc2abff7ec03c235c4ab
Chris-Pedersen <c.pedersen@nyu.edu>
2024-02-26 10:24:05 -0500

    Going to create seperate object for denoiser performance

--- pyqg_explorer/performance/emulator_performance.py
+++ pyqg_explorer/performance/emulator_performance.py
@@ -8,7 +8,7 @@ import xarray as xr
 
 class EmulatorPerformance():
     """ Object to store performance tests relevant to neural emulators """
-    def __init__(self,network,denoiser=None,denoise_timestep=None):
+    def __init__(self,network):
         """ network:  Torch model we want to test. Assuming this is a model for the subgrid forcing
             valid_loader: validation loader from EmulatorDatasetTorch """
         
@@ -19,10 +19,6 @@ class EmulatorPerformance():
             self.device="cpu"
         
         self.network=network.to(self.device)
-        self.denoiser=denoiser
-        self.denoise_timestep=denoise_timestep
-        if self.denoiser:
-            self.denoiser.to(self.device)
         """ Are we using eddy or jet """
         if network.config["eddy"]:
             self.eddy="eddy"
@@ -48,7 +44,6 @@ class EmulatorPerformance():
         for aa in range(20):
             mses=np.empty(len(times))
             mses_0=np.empty(len(times))
-            mses_dn=np.empty(len(times))
             q_i=self.normalise(torch.tensor(ds.q[aa,0].to_numpy(),device=self.device,dtype=torch.float32))
             ## Index counter for loss arrays
             cc=0
@@ -56,10 +51,6 @@ class EmulatorPerformance():
             for bb in range(self.network.config["increment"],ds.q.shape[1],self.network.config["increment"]):
                 q_i_dt=self._get_next_step(q_i)
                 q_i_true=self.normalise(torch.tensor(ds.q[aa,bb].values,device=self.device))
-                if self.denoiser:
-                    q_i_dn=self.denoiser.denoising(q_i_dt.unsqueeze(0),self.denoise_timestep)
-                    q_i_dn=q_i_dn.squeeze()
-                    mses_dn[cc]=(criterion(q_i_true,q_i_dn))
                 mses[cc]=(criterion(q_i_true,q_i_dt))
                 mses_0[cc]=(criterion(q_i_true,init))
                 q_i=q_i_dt
@@ -68,8 +59,6 @@ class EmulatorPerformance():
             plt.title("MSE(truth,emulator) in blue, MSE(truth at t=0, truth at t=i) in red")
             plt.plot(times,mses,color="blue",label="Emulator MSE wrt truth",alpha=0.2)
             plt.plot(times,mses_0,color="red",label="True MSE wrt t=0",alpha=0.2)
-            if self.denoiser:
-                plt.plot(times,mses_dn,color="yellow",label="True MSE wrt t=0",alpha=0.2)
             plt.yscale("log")
             plt.ylim(1e-3,5e0)
             plt.xlabel("timestep")
@@ -84,4 +73,3 @@ class EmulatorPerformance():
         x_lower = transforms.normalise_field(q[1],self.network.config["q_mean_lower"],self.network.config["q_std_lower"])
         x = torch.stack((x_upper,x_lower),dim=0)
         return x
-    
